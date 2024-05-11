e61e5576d0b3f7e17fe2297920e4ae55faa3a6bc
zeynep <uyslzynp78@gmail.com>
2024-05-11 13:41:00 +0300

    .

---
+++ shopping.data/Concrete/efStoreRepository.cs
@@ -0,0 +1,19 @@
+// using shopping.data.Abstract;
+
+// namespace shopping.data.Concrete;
+
+// public class efStoreRepository : IStoreRepository
+// {
+//     private StoreDbContext _context;
+
+//     public efStoreRepository(StoreDbContext context)
+//     {
+//         _context=context;
+//     }
+//     public IQueryable<Product>Products =>_context.Products;
+
+//     public void CreateProduct(Product entity)
+//     {
+//         throw new NotImplementedException();
+//     }
+// }
